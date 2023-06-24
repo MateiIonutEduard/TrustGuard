ff71523dc192a21f01b8d9b8c7d5fcf5073f6533
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-24 12:10:38 +0300

    Add app base point for each application

---
+++ Data/BasePoint.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable
+
+namespace TrustGuard.Data
+{
+    public class BasePoint
+    {
+        public int Id { get; set; }
+        public string x { get; set; }
+        public string y { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool? IsDeleted { get; set; }
+        public int ApplicationId { get; set; }
+        public int DomainId { get; set; }
+    }
+}
