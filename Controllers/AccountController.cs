7200bd49979dd750dbe278e7cfc2f487230b2ad4
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-25 12:01:05 +0300

    Add login page view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrustGuard.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AccountRequestModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TrustGuard.Models
+{
+	public class AccountRequestModel
+	{
+	}
+}
