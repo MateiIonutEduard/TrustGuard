ec7e36153435b561f46bd1d8663334579be63ea9
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-25 12:18:10 +0300

    Add account response model

---
+++ Models/AccountResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrustGuard.Models
+{
+	public class AccountResponseModel
+	{
+		public int? id { get; set; }
+		public string? username { get; set; }
+		public bool? address { get; set; }
+		public int status { get; set; }
+	}
+}
