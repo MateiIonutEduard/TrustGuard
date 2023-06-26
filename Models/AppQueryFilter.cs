a4b7bfa1263512a86156938b73908bc8c91e6270
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-26 19:20:21 +0300

    Modify project query layout

---
+++ Models/AppQueryFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace TrustGuard.Models
+{
+    public class AppQueryFilter
+    {
+        public string? AppName { get; set; }
+        public int ActivityType { get; set; }
+        public int AppType { get; set; }
+    }
+}
