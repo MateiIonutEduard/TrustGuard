77098c4cb30c246fcf521478d359a0af17c0858d
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-28 11:34:59 +0300

    Add token factory

---
+++ Environment/TokenFactory.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace TrustGuard.Environment
+{
+	public class TokenFactory
+	{
+		public TokenFactory()
+		{ }
+
+		/* require header payload to JWT tokens */
+		public string GetHeader()
+		{
+			var header = new Dictionary<string, string>
+			{
+				{ "alg", "TG256" },
+				{ "typ", "JWT" }
+			};
+
+			string content = JsonConvert.SerializeObject(header, Formatting.Indented);
+			byte[] data = Encoding.ASCII.GetBytes(content);
+			return Convert.ToBase64String(data);
+		}
+	}
+}
