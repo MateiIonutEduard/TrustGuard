a5d62cef68c63415928eabbe5ab4cce537c09dd5
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-06-28 11:09:14 +0300

    Add claim type model

---
+++ Environment/ClaimType.cs
@@ -0,0 +1,12 @@
+﻿namespace TrustGuard.Environment
+{
+	public class ClaimType
+	{
+		public static readonly string Issuer = "iss";
+		public static readonly string Subject = "sub";
+		public static readonly string Email = "email";
+		public static readonly string Expires = "exp";
+		public static readonly string Audience = "aud";
+		public static readonly string Jti = "jti";
+	}
+}
