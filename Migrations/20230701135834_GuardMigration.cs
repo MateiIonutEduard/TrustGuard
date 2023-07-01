e6b7f6647d6d72dbcd57fc2633bd5a2d6994b37f
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-01 16:59:17 +0300

    Add new database migration

---
+++ Migrations/20230701135834_GuardMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrustGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuardMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRevoked",
+                table: "KeyPair",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ValidateLifetime",
+                table: "KeyPair",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRevoked",
+                table: "KeyPair");
+
+            migrationBuilder.DropColumn(
+                name: "ValidateLifetime",
+                table: "KeyPair");
+        }
+    }
+}
