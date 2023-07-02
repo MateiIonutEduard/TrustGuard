2c9480a9ec4000ef20cf4c360c3f3bdef47f2102
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-02 13:39:32 +0300

    Add how to use help to dialog #15

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function () {
     $('#description').summernote();
 });
+
+function CopyTo(uid) {
+    var authText = document.getElementById(uid);
+    authText.select();
+
+    authText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(authText.value);
+}
