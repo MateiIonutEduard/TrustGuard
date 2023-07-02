d195b5c5173d2afff49b0539a6d73f6e25b514b9
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-02 16:35:32 +0300

    Add log system entity, used in MongoDB server #15

---
+++ Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TrustGuard.Models
+{
+    public class Log
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int LogLevel { get; set; }
+    }
+}
