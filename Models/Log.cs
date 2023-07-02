7826d99bc5acecac146ae01655774fb40065e1ea
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-02 17:48:51 +0300

    Add log creation and removing features #15

--- Models/Log.cs
+++ Models/Log.cs
@@ -9,6 +9,7 @@ namespace TrustGuard.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string? Message { get; set; }
+        public string? Webcode { get; set; }
         public DateTime CreatedAt { get; set; }
         public int LogLevel { get; set; }
     }
--- Services/LogService.cs
+++ Services/LogService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using TrustGuard.Models;
+using LogLevel = TrustGuard.Models.LogLevel;
 
 namespace TrustGuard.Services
 {
@@ -14,5 +15,25 @@ namespace TrustGuard.Services
             var mongoDatabase = mongoClient.GetDatabase(bridgeWaterSettings.Value.DatabaseName);
             logs = mongoDatabase.GetCollection<Log>(bridgeWaterSettings.Value.CollectionName);
         }
+
+        public async Task CreateLogAsync(string body, string? webcode, LogLevel logLevel)
+        {
+            Log log = new Log
+            {
+                Message = body,
+                CreatedAt = DateTime.UtcNow,
+                LogLevel = (int)logLevel
+            };
+
+            /* warn user account if something is suspicious */
+            if (!string.IsNullOrEmpty(webcode) && logLevel == LogLevel.Warning)
+                log.Webcode = webcode;
+
+            /* save log at MongoDB database */
+            await logs.InsertOneAsync(log);
+        }
+
+        /* remove log by specific id */
+        public async Task RemoveLogAsync(string id) => await logs.DeleteOneAsync(x => x.Id == id);
     }
 }
