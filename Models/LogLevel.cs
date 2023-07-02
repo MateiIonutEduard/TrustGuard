a909459305cff362986e6aa231c14eaff414de3f
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-02 17:30:40 +0300

    Add log level enum #15

---
+++ Models/LogLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace TrustGuard.Models
+{
+    public enum LogLevel : int
+    {
+        Fatal = 1, 
+        Error = 2,
+        Danger = 3, 
+        Warning = 4,
+        Info = 5, 
+        All = 6
+    }
+}
