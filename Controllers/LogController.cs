5e17717d7bcbf7e72d7c812c9d3b7e5e32da767c
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-02 22:47:05 +0300

    Add log removing feature #15

---
+++ Controllers/LogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrustGuard.Services;
+
+namespace TrustGuard.Controllers
+{
+    public class LogController : Controller
+    {
+        readonly LogService logService;
+
+        public LogController(LogService logService)
+        { this.logService = logService; }
+
+        [HttpPost, Authorize]
+        public async Task<IActionResult> Index(int appId, string logId)
+        {
+            string? userId = HttpContext.User?.Claims?
+                .FirstOrDefault(u => u.Type == "id")?.Value;
+
+            // remove when user is authenticated
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await logService.RemoveLogAsync(logId);
+                return Redirect($"/Home/Details/?id={appId}");
+            }
+            else
+                return Redirect("/Account/");
+        }
+    }
+}
