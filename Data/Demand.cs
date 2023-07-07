37bb4bf3debfaebd3fe7df086a462f162695528a
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-07 09:46:03 +0300

    Add new custom fields at demand entity
    Add new custom fields at demand entity.

--- Data/Demand.cs
+++ Data/Demand.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public DateTime IssuedAt { get; set; }
         public bool? IsSeen { get; set; }
+        public DateTime? SolvedAt { get; set; }
+        public int TotalSeconds { get; set; }
     }
 }
