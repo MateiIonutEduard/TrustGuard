b2999af0c8c42c22a2a247d8915070bcc47839ca
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-07 09:48:09 +0300

    Add final database migration
    Add final database migration with demand entity.

---
+++ Migrations/20230707064717_FinalMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrustGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SolvedAt",
+                table: "Demand",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalSeconds",
+                table: "Demand",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SolvedAt",
+                table: "Demand");
+
+            migrationBuilder.DropColumn(
+                name: "TotalSeconds",
+                table: "Demand");
+        }
+    }
+}
