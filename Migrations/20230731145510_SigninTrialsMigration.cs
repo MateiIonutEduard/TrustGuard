f276f0c38c1e89d8e415ef5eae95f3e21976e51c
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-07-31 17:56:37 +0300

    Add signin trials migration
    Add signin trials migration, to mantains database update.

---
+++ Migrations/20230731145510_SigninTrialsMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrustGuard.Migrations
+{
+    /// <inheritdoc />
+    public partial class SigninTrialsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SigninTrials",
+                table: "Account",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SigninTrials",
+                table: "Account");
+        }
+    }
+}
